005718b14dfb4c46a499d5778f60264c8e552649
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-01-17 20:26:12 +0100

    basic development IdentityServer set up

--- mintvattAPI/mintattIdentityServer/Startup.cs
+++ mintvattAPI/mintattIdentityServer/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace mintattIdentityServer
 {
@@ -16,10 +19,30 @@ namespace mintattIdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework;trusted_connection=yes;";
+            //var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetAllApiResources())
                 .AddInMemoryClients(Config.GetUsers());
+
+                //.AddConfigurationStore(options =>
+                //{
+                //    options.ConfigureDbContext = builder =>
+                //        builder.UseSqlServer(connectionString,
+                //            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                //})
+                //.AddOperationalStore(options =>
+                //{
+                //    options.ConfigureDbContext = builder =>
+                //        builder.UseSqlServer(connectionString,
+                //            sql => sql.MigrationsAssembly(migrationsAssembly));
+                //});
+            
+
+                //.AddInMemoryApiResources(Config.GetAllApiResources())
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
