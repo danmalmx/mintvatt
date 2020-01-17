e55d6b4654f174ab2937da9bdd0270fc23c2a35f
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-01-17 14:49:43 +0100

    adding user API

---
+++ mintvattAPI/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace mintvattAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [DefaultValue("Email")]
+        public string UserName { get; set; } 
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string PasswordConfirmation { get; set; }
+
+    }
+}
---
+++ mintvattAPI/Models/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace mintvattAPI.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
