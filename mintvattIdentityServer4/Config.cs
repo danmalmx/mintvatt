31d2fb3464238f32338cb06a9d9361ba724c67d9
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-02-04 11:12:09 +0100

    adding TestUser for trial-run

--- mintvattIdentityServer4/Config.cs
+++ mintvattIdentityServer4/Config.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 
 namespace mintvattIdentityServer4
 {
     public class Config
     {
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "Danmalmx",
+                    Password = "1234"
+                },
+                   new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "Bobster",
+                    Password = "1234"
+                },
+            };
+        }
+   
+
         public static IEnumerable<ApiResource> GetAllApiResources()
         {
             return new List<ApiResource>
